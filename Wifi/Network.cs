e97db1c8c65c9bad04dc40b315b3933d19ae7a44
ShmatkovD <client.pas.95@gmail.com>
2015-03-29 06:29:19 +0300

    Added class Network.

--- Wifi/Network.cs
+++ Wifi/Network.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,26 @@ namespace WifiCatcherDesktop.Wifi
 {
     public class Network
     {
+        private Dictionary<int, int> _values;
+
+        public string Ssid { get; private set; }
+        public bool IsFree { get; private set; }
+
+        public Dictionary<int, int> Values
+        {
+            get { return _values; }
+        }
+
+        public Network(string ssid, bool isFree)
+        {
+            this.Ssid = ssid;
+            this.IsFree = isFree;
+            _values = new Dictionary<int, int>();
+        }
+
+        public void AddValue(int angle, int level)
+        {
+            _values.Add(angle, level);
+        }
     }
 }
