4c75249d9fe1fb53e79c6c7623e6213302f34dda
Алексей Лапотко <alexej.lapotko@gmail.com>
2015-03-29 04:33:13 +0300

    ServoController became Controller

---
+++ Arduino/CatcherState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WifiCatcherDesktop.Arduino
+{
+    public enum CatcherState
+    {
+        None = 0, Scanning = 1, NetworkFound = 2, NetworkNotFound = 3
+    }
+}
