94cc57fa3d505de4a482cec7458dc11904f7af52
Алексей Лапотко <alexej.lapotko@gmail.com>
2015-03-29 03:00:20 +0300

    Created frames for Wifi classes

---
+++ Wifi/Connector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WifiCatcherDesktop.Wifi
+{
+    public class Connector
+    {
+        public void Connect(Network network)
+        {
+            
+        }
+    }
+}
