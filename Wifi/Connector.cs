153a80907f6178be2c3238bdb5c74ba955a57ed7
ShmatkovD <client.pas.95@gmail.com>
2015-03-29 14:01:53 +0300

    Made connector for free wifi without password.

--- Wifi/Connector.cs
+++ Wifi/Connector.cs
@@ -1,16 +1,34 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NativeWifi;
 
 namespace WifiCatcherDesktop.Wifi
 {
     public class Connector
     {
+        private static readonly Guid AdapterGuid = new Guid("{7ae830fe-f14c-486d-836a-d6fb96da9854}");
+
+        private WlanClient.WlanInterface GetAdapterWlanInterface()
+        {
+            var client = new WlanClient();
+            return client.Interfaces.FirstOrDefault(wlanIface => wlanIface.InterfaceGuid == AdapterGuid);
+        }
+
         public void Connect(Network network)
         {
-            
+            WlanClient.WlanInterface wlanIface = GetAdapterWlanInterface();
+            if (network.IsFree)
+            {
+                string profileName = network.Ssid;
+                string profileXml = string.Format("<?xml version=\"1.0\" encoding=\"US-ASCII\"?><WLANProfile xmlns=\"http://www.microsoft.com/networking/WLAN/profile/v1\"><name>{0}</name><SSIDConfig><SSID><name>{0}</name></SSID></SSIDConfig><connectionType>ESS</connectionType><connectionMode>auto</connectionMode><autoSwitch>false</autoSwitch><MSM><security><authEncryption><authentication>open</authentication><encryption>none</encryption><useOneX>false</useOneX></authEncryption></security></MSM></WLANProfile>", profileName);
+                wlanIface.SetProfile(Wlan.WlanProfileFlags.AllUser, profileXml, true);
+                wlanIface.Connect(Wlan.WlanConnectionMode.Profile, Wlan.Dot11BssType.Any, profileName);
+            }
         }
     }
 }
