268f77e6d800cc8c252f0b9aa7af3300a24362b8
ShmatkovD <client.pas.95@gmail.com>
2015-03-29 06:40:46 +0300

    Added class Base which contains database of networks.

--- Wifi/Base.cs
+++ Wifi/Base.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,11 +9,36 @@ namespace WifiCatcherDesktop.Wifi
 {
     public class Base
     {
-        public List<Network> Networks;
+        private List<Network> _networks; 
+
+        public List<Network> Networks
+        {
+            get { return _networks; }
+        }
 
         public void InsertOrUpdate(Network network)
         {
-            
+            Network itemForUpdate = null;
+            foreach (var item in _networks)
+            {
+                if (item.Ssid == network.Ssid)
+                {
+                    itemForUpdate = item;
+                }
+            }
+            if (itemForUpdate == null)
+            {
+                _networks.Add(network);
+            }
+            else
+            {
+                foreach (var value in network.Values)
+                {
+                    itemForUpdate.AddValue(value.Key, value.Value);
+                }
+            }
         }
+
+
     }
 }
